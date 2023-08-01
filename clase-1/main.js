61a386115a82a8b492430b7c7db2d1550b3c8c9b
raulgm00 <raulgm00@gmail.com>
2023-08-01 08:57:44 -0600

    Se reestructura las carpetas y proyectos de rama principal

--- clase-1/main.js
+++
@@ -1,4 +0,0 @@
-let numero_uno= 15;
-let numero_dos= 20;
-
-console.log ( 'La suma de los numeros es: ' + numero_uno + numero_dos);
---
+++ clase-3/main.js
@@ -0,0 +1,3 @@
+const prompt = require("prompt-sync")({ sigint: true });
+let nombre= prompt("Ingresa tu nombre: ");
+console.log(`hola ${nombre}`)
