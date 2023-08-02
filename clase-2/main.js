9e5642b525573bddfa70f8bb38e7983032691bf4
raulgm00 <raulgm00@gmail.com>
2023-08-02 17:25:21 -0600

    EJercicio clase No.2

--- clase-2/main.js
+++ clase-2/main.js
@@ -1,7 +1,8 @@
 const prompt = require("prompt-sync")({ sigint: true });
-let numero_uno= prompt("Ingrese su primer numero: ");
-console.log(`Numero 1:  ${numero_uno}`);
-let numero_dos= prompt("Ingrese su segundo numero: ");
-console.log(`Numero 2: ${numero_dos}`);
-console.log (`La suma de los numeros es: ${numero_uno} + ${numero_dos}`);
-console.log (numero_uno + numero_dos );
+let numero_uno=  parseInt(prompt("Ingrese su primer numero: "));
+let numero_dos=  parseInt(prompt("Ingrese su segundo numero: "));
+let suma =  numero_uno + numero_dos;
+
+
+console.log ("La suma de los numeros es:" + suma );
+
---
+++ clase-4/main.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")({ sigint: true });
+let nombre= prompt("Ingresa tu nombre: ");
+console.log(`hola ${nombre}`)
+
+// ANCHORS 
