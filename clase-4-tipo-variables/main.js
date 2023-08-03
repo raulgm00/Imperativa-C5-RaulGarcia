8634f8dad57b6b248dc43ae00346d058bc80c2a7
raulgm00 <raulgm00@gmail.com>
2023-08-03 13:05:30 -0600

    Renombre de proyectos

---
+++ clase-4-tipo-variables/main.js
@@ -0,0 +1,31 @@
+// const prompt = require("prompt-sync")({ sigint: true });
+// let nombre= prompt("Ingresa tu nombre: ");
+// console.log(`hola ${nombre}`)
+
+// EJEMPLO 1
+let unNumero = 123;
+console.log(unNumero); // imprimirá el valor asignado a unNumero, o sea 123
+
+
+// EJEMPLO 2
+// Esto es una asignación estática (valor fijo)
+let unNumero_2 = 123;
+// Esto es una asignación dinámica (resultado de una operacion)
+let siguienteNumero = unNumero_2 + 1;
+// Imprime en pantalla el valor asignado, o sea 124
+console.log(siguienteNumero);
+
+
+//EJEMPLO 3
+let primerNumero = 100
+let segundoNumero = 5
+
+let resultadoSuma = primerNumero + segundoNumero // 105
+let resultadoResta = primerNumero - segundoNumero // 95
+let resultadoMultiplicacion = primerNumero * segundoNumero // 500
+let resultadoDivision = primerNumero / segundoNumero // 20
+
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoMultiplicacion);
+console.log(resultadoDivision);
--- clase-4/main.js
+++
@@ -1,5 +0,0 @@
-const prompt = require("prompt-sync")({ sigint: true });
-let nombre= prompt("Ingresa tu nombre: ");
-console.log(`hola ${nombre}`)
-
-// ANCHORS 
